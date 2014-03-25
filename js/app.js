ddab67257bad54b84b55acf928e01052f2ec15bc
Derek Lieu <derek@developmentseed.org>
2014-03-25 14:47:51 -0400

    removing unnecessary js and mocks files

--- js/app.js
+++
@@ -1,9 +0,0 @@
-$(function() {
-    'use strict';
-
-    var views = ['MainStoryView', 'MapView', 'PaginatedView', 'ReportSearch', 'AppView' ];
-    _.each(views, function(view) {
-        new app[view]();
-    });
-
-});
--- js/events.js
+++
@@ -1,3 +0,0 @@
-(function ($) {
-    app.events = _.extend({}, Backbone.Events);
-})(jQuery);
